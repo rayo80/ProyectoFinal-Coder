08bdbd4fcd08c3d1e634f2de3dd088d6cd2ef495
Sergio <sergiovilla242@gmail.com>
2022-11-21 22:14:25 -0500

    change budget

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/1-pfvilla'),
+      dir: require('path').join(__dirname, './coverage/pfvilla'),
       subdir: '.',
       reporters: [
         { type: 'html' },
